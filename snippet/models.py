0ce2f9acb6f56c6d9188b934b087cdf34b92cfbe
lnxpy <lnxpylnxpy@gmail.com>
2020-03-27 20:04:03 +0430

    model translation submitted

--- snippet/models.py
+++ snippet/models.py
@@ -9,6 +9,7 @@ from django.utils.translation import gettext as _
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
 COLOR_THEME = [
     ('suggest1',_('red-pink')),
     ('suggest2',_('red-yellow')),
@@ -17,22 +18,22 @@ COLOR_THEME = [
 ]
 
 class Snippet(models.Model):
-    SID = models.CharField(max_length=10, verbose_name='شناسه')
-    title = models.CharField(max_length=100, verbose_name='عنوان')
-    detail = models.TextField(blank=True, verbose_name='توضیحات')
-    script = models.TextField(verbose_name='اسکریپت')
-    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100, verbose_name='زبان برنامه نویسی')
-    pub_date = models.CharField(max_length=50, verbose_name='تاریخ ثبت')
-    link = models.URLField(verbose_name='آدرس')
+    SID = models.CharField(max_length=10, verbose_name=_('SID'))
+    title = models.CharField(max_length=100, verbose_name=_('Title'))
+    detail = models.TextField(blank=True, verbose_name=_('Details'))
+    script = models.TextField(verbose_name=_('Script'))
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100, verbose_name=_('Programming language'))
+    pub_date = models.CharField(max_length=50, verbose_name=_('Submition date'))
+    link = models.URLField(verbose_name=_('URL Address'))
 
     def __str__(self):
         return '%s ~~ %s ~~ %s'%(self.SID, self.title, self.pub_date)
 
 class Suggest(models.Model):
-    title = models.CharField(max_length=30, verbose_name='عنوان')
-    content = RichTextField(verbose_name='محتوا')
-    theme = models.CharField(choices=COLOR_THEME, max_length=100, verbose_name='نگارش', default='suggest1')
-    pub_date = models.CharField(max_length=50, default=calendar(), verbose_name='تاریخ ثبت')
+    title = models.CharField(max_length=30, verbose_name=_('Title'))
+    content = RichTextField(verbose_name=_('Content'))
+    theme = models.CharField(choices=COLOR_THEME, max_length=100, verbose_name=_('Style'), default='suggest1')
+    pub_date = models.CharField(max_length=50, default=calendar(), verbose_name=_('Submition date'))
 
     def __str__(self):
         return self.title
