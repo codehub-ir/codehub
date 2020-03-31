181ea44ee9425acf4872750e725eb13e1cbb1691
lnxpy <lnxpylnxpy@gmail.com>
2020-03-31 16:37:37 +0430

    Teammate model is enabled on the Admin panel now

--- snippet/admin.py
+++ snippet/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Snippet, Suggest
+from .models import Snippet, Suggest, Teammate
 
 
 admin.site.register(Snippet)
 admin.site.register(Suggest)
+admin.site.register(Teammate)
