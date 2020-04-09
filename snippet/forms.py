adbb26ad031b2de4545f8af6ac40fa179473f83f
lnxpy <lnxpylnxpy@gmail.com>
2020-04-09 16:22:40 +0430

    error field added

--- snippet/forms.py
+++ snippet/forms.py
@@ -6,4 +6,4 @@ class SnippetForm(forms.ModelForm):
 
     class Meta:
         model = Snippet
-        fields = ('title', 'detail', 'script', 'language',)
+        fields = ('title', 'detail', 'script', 'error', 'language',)
