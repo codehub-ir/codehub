385ae3bd35feb11197f5601386c20eaabadd7813
lnxpy <lnxpylnxpy@gmail.com>
2020-04-12 16:23:08 +0430

    API structure reset

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('v1/snippet/<str:SID>', views.DetailSnippet.as_view()),
-    path('v1/snippet/', views.AddSnippet.as_view()),
-    path('v1/team/', views.TeamList.as_view()),
-]
