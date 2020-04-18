d0d3799336a56ea75e838def5d6567ca06dc1149
lnxpy <lnxpylnxpy@gmail.com>
2020-04-18 22:03:56 +0430

    copying to clipboard source wrote

--- snippet/static/snippet/js/main.js
+++ snippet/static/snippet/js/main.js
@@ -1,12 +1,24 @@
 // scroll functions
-$(window).scroll(function(e) {
+$(window).scroll(function (e) {
 
-// add/remove class to navbar when scrolling to hide/show
-var scroll = $(window).scrollTop();
-if (scroll >= 150) {
-    $('.navbar').addClass("navbar-hide");
-} else {
-    $('.navbar').removeClass("navbar-hide");
-}
+    // add/remove class to navbar when scrolling to hide/show
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+        $('.navbar').addClass("navbar-hide");
+    } else {
+        $('.navbar').removeClass("navbar-hide");
+    }
 
 });
+
+function copyElementText(id) {
+    var text = document.getElementById(id).innerText;
+    var elem = document.createElement("textarea");
+    var website_snippet_address = 'http://codehub.pythonanywhere.com/snippet/';
+    document.body.appendChild(elem);
+    elem.value = website_snippet_address + text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+    window.alert('Snippet link copied to clipboard!')
+}
