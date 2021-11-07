0badf8abe269f82bbe88ad801769cc5d8b24e91f
lnxpy <lnxpylnxpy@gmail.com>
2021-11-07 04:16:12 +0330

    get_abs_url method added for Snippet + model fields clearified

--- main/models.py
+++ main/models.py
@@ -1,26 +1,43 @@
 from django.db import models
+from django.urls import reverse
 from .constants import LANGUAGES
 from .utils import generateSID
 
 
 class Snippet(models.Model):
 
-    id = models.CharField('ID', max_length=11,
-                          primary_key=True, editable=False)
+    id = models.CharField(
+        verbose_name='ID',
+        max_length=11,
+        primary_key=True,
+        editable=False,
+    )
+    title = models.CharField(
+        max_length=50,
+    )
+    description = models.TextField()
+    body = models.TextField(
+        verbose_name='Script',
+    )
+    lang = models.CharField(
+        verbose_name='Language',
+        max_length=250,
+        choices=LANGUAGES,
+    )
+    created_on = models.DateField(
+        auto_now=True,
+        editable=False,
+    )
 
-    title = models.CharField(max_length=50, default='')
-    description = models.TextField(default='')
-    body = models.TextField('Script', default='')
-    lang = models.CharField('Language', max_length=250,
-                            choices=LANGUAGES, default='')
-    created_on = models.DateField(auto_now=True, editable=False)
-
-    # TODO: Adding view counter using django-hitcount
+    # TODO: Adding a view counter using django-hitcount pkg
 
     class Meta:
-        pass
+        indexes = [models.Index(fields=['id'])]
 
     def __str__(self): return self.title
+
+    def get_absolute_url(self):
+        return reverse('snippet_detail', args=[self.id])
 
     def save(self, *args, **kwargs):
         self.id = generateSID()
