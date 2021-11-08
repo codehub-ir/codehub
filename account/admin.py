86efd7dcecf16496bcc2216cae0e9a545e1dd8a9
lnxpy <lnxpylnxpy@gmail.com>
2021-11-09 02:41:07 +0330

    custom admin section designed for users

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
+
+
+class UserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = ['username', 'email']
+
+
+admin.site.register(User, UserAdmin)
