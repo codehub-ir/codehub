8b493edd11924307506c13a0db71dd2956ff229f
lnxpy <lnxpylnxpy@gmail.com>
2021-11-09 02:42:18 +0330

    simple User model designed

--- account/models.py
+++ account/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    display_name = models.CharField(
+        max_length=50,
+    )
+    email = models.EmailField(
+        blank=True,
+    )
+    twitter = models.EmailField(
+        blank=True,
+    )
+    github = models.EmailField(
+        blank=True,
+    )
+
+    def __str__(self): return self.username
