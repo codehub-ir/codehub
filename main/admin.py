991eba90a21e118b30cc25dcd3f2a3ff18897403
lnxpy <lnxpylnxpy@gmail.com>
2021-11-10 01:11:22 +0330

    all models being registered in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Snippet
+from .models import Snippet, Ticket, Tag, Comment
 
 
 admin.site.register(Snippet)
+admin.site.register(Ticket)
+admin.site.register(Tag)
+admin.site.register(Comment)
