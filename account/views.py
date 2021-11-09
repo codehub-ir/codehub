5151aec02f905003a245e003c5885f2e82f745ba
lnxpy <lnxpylnxpy@gmail.com>
2021-11-09 03:40:44 +0330

    ProfileView and SignUpView created

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
-# Create your views here.
+from .forms import CustomUserCreationForm, CustomUserUpdateForm
+from .models import User
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = CustomUserUpdateForm
+    template_name = "profile.html"
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, *args, **kwargs):
+        user = User.objects.get(username=self.request.user)
+        return user
