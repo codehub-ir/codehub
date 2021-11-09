f1dde983a3df9a70129c380e10f8fe2d14034e9f
lnxpy <lnxpylnxpy@gmail.com>
2021-11-09 03:40:19 +0330

    profile path added

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, ProfileView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
