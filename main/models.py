e36c16fe39ce8612d791c8cad1b66379145c2771
lnxpy <lnxpylnxpy@gmail.com>
2021-11-10 05:50:14 +0330

    TODO item added to implement get_absolute_url method after URLs implementations

--- main/models.py
+++ main/models.py
@@ -45,8 +45,7 @@ class Snippet(models.Model):
 
     def __str__(self): return self.title
 
-    def get_absolute_url(self):
-        return reverse('snippet_detail', args=[self.id])
+    # TODO: Implementing get_absolute_url method
 
     def save(self, *args, **kwargs):
         self.id = generateSID()
