2a6cfc1a7fe0883a15c1833b4033ce806dfcb560
lnxpy <lnxpylnxpy@gmail.com>
2021-11-11 08:33:06 +0330

    Ticket test cases created

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -1,8 +1,10 @@
+from django.db.models import fields
 from django.test import TestCase
 
-from main.models import Snippet, Tag
+from main.models import Snippet, Tag, Ticket
 from main.constants import LANGUAGES
 from main.utils import generateSID
+from account.models import User
 
 
 class SnippetTestCase(TestCase):
@@ -17,18 +19,21 @@ class SnippetTestCase(TestCase):
         }
 
     def setUp(self):
-        Snippet.objects.create(**self.sample)
+        self.snippet = Snippet.objects.create(**self.sample)
 
     def test_if_obj_returns_same_fields(self):
-        snippet = Snippet.objects.get(**self.sample)
         fields = {
-            'title': snippet.title,
-            'description': snippet.description,
-            'body': snippet.body,
-            'lang': snippet.lang,
+            'title': self.snippet.title,
+            'description': self.snippet.description,
+            'body': self.snippet.body,
+            'lang': self.snippet.lang,
         }
-
         self.assertEqual(fields, self.sample)
+
+    def test_sid_length(self):
+        # nbyte is set to 5 by default
+        # which provides an id with 10 chars
+        self.assertEqual(len(self.snippet.id), 10)
 
 
 class TagTestCase(TestCase):
@@ -41,20 +46,67 @@ class TagTestCase(TestCase):
         }
 
     def setUp(self):
-        Tag.objects.create(**self.sample)
+        self.tag = Tag.objects.create(**self.sample)
 
     def test_if_obj_returns_same_fields(self):
-        tag = Tag.objects.get(**self.sample)
         fields = {
-            'name': tag.name,
-            'description': tag.description,
+            'name': self.tag.name,
+            'description': self.tag.description,
         }
 
         self.assertEqual(fields, self.sample)
 
 
-def TicketTestCase(TestCase):
+class TicketTestCase(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        pass
+        cls.ticket_sample = {
+            'title': 'Ticket Title',
+            'description': 'Ticket Description',
+        }
+
+        cls.user_sample = {
+            'username': 'johndoe',
+            'display_name': 'John Doe',
+            'email': 'john@doe.com',
+            'password': 'testPass123',
+        }
+
+        cls.tag_sample = {
+            'name': 'Tag Name',
+            'description': 'Tag Description',
+        }
+
+    def setUp(self):
+        _user = User.objects.create(**self.user_sample)
+
+        '''
+        Since the Ticket.tags is a ManyToMany field,
+        It needs to get saved eventualy right after
+        It created.
+        '''
+        _tag = Tag(**self.tag_sample)
+        _tag.save()
+
+        self.ticket = Ticket.objects.create(**self.ticket_sample)
+        self.ticket.save()
+
+        '''
+        Then we add those pre-build Tab obj(s) to out
+        Ticket object.
+        '''
+        self.ticket.tags.set([_tag])
+        self.ticket.created_by = _user
+
+    def test_obj_returns_same_fields(self):
+        fields = {
+            'title': self.ticket.title,
+            'description': self.ticket.description,
+        }
+
+        self.assertEqual(fields, self.ticket_sample)
+
+    def test_check_ticket_creator(self):
+        self.assertEqual(self.ticket.created_by.username,
+                         self.user_sample['username'])
