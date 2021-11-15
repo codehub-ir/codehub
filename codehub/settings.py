ca57c7ac03115248cae3b6b06b8eb3889b678e80
lnxpy <lnxpylnxpy@gmail.com>
2021-11-15 21:30:09 +0330

    ALLOWED_HOSTS config issue fixed

--- codehub/settings.py
+++ codehub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [config('ALLOWED_HOST')]
+ALLOWED_HOSTS = ['codehub.pythonanywhere.com']
 
 
 # Application definition
