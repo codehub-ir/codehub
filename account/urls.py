1f96a05b747571f0c06fd5aca2a5aebcb15fa3ee
lnxpy <lnxpylnxpy@gmail.com>
2021-11-15 21:27:50 +0330

    change password custom url added

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import ProfileView, SignUpView
+from .views import ProfileView, SignUpView, CustomPasswordChangeView
+
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/', ProfileView.as_view(), name='profile'),
+
+    path('password/', CustomPasswordChangeView.as_view(), name='reset_password')
 ]
