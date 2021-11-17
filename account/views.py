a71bb53120966d5be7e6747a9a7837793c8c32b6
lnxpy <lnxpylnxpy@gmail.com>
2021-11-17 03:34:24 +0330

    CustomPasswordChangeView view created

--- account/views.py
+++ account/views.py
@@ -1,5 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.views import PasswordChangeView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import CustomUserCreationForm, CustomUserUpdateForm
 from .models import User
@@ -11,7 +14,7 @@ class SignUpView(CreateView):
     template_name = 'registration/signup.html'
 
 
-class ProfileView(UpdateView):
+class ProfileView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = CustomUserUpdateForm
     template_name = "profile.html"
@@ -20,3 +23,8 @@ class ProfileView(UpdateView):
     def get_object(self, *args, **kwargs):
         user = User.objects.get(username=self.request.user)
         return user
+
+
+class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'registration/password_change.html'
+    success_url = reverse_lazy('home')
