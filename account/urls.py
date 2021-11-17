1e4a6cb597f94e2e1f2e0071fafd833771ac9e50
lnxpy <lnxpylnxpy@gmail.com>
2021-11-17 03:33:27 +0330

    CustomPasswordChangeView view url added

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProfileView, SignUpView
+from .views import ProfileView, SignUpView, CustomPasswordChangeView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('password/', CustomPasswordChangeView.as_view(), name='change_password'),
 ]
