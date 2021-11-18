4802db5fb538a0622d1d1645848a0d4b2b0ab44e
lnxpy <lnxpylnxpy@gmail.com>
2021-11-19 02:41:06 +0330

    Static and Media urls and roots added

--- codehub/settings.py
+++ codehub/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
