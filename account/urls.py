a622b804eabbf6daf4798dfae5a993273c7f965b
lnxpy <lnxpylnxpy@gmail.com>
2021-11-20 01:10:47 +0330

    custom login path added

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CustomPasswordChangeView, ProfileView, SignUpView
+from .views import CustomPasswordChangeView, ProfileView, SignUpView, CustomLoginView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('password/', CustomPasswordChangeView.as_view(), name='change_password'),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
