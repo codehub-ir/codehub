b506a74ac6af667b1cd6ab759430f5c0205c75b0
lnxpy <lnxpylnxpy@gmail.com>
2021-11-19 05:48:15 +0330

    User fields translated

--- account/models.py
+++ account/models.py
@@ -1,17 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from django.utils.translation import gettext as _
+
 
 class User(AbstractUser):
 
     display_name = models.CharField(
+        _('Display Name'),
         max_length=50,
     )
-    email = models.EmailField()
+    email = models.EmailField(
+        _('Email Address'),
+    )
     twitter = models.URLField(
+        _('Twitter Address'),
         blank=True,
     )
     github = models.URLField(
+        _('Github Address'),
         blank=True,
     )
 
