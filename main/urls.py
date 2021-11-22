286a5e392a9cb3075028f99a775704d2c97090eb
lnxpy <lnxpylnxpy@gmail.com>
2021-11-22 13:43:21 +0330

    Snippet create and Snippet detail urls added

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import HomeView
+from .views.template import HomeView, SnippetCreateView, SnippetView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('new/', SnippetCreateView.as_view(), name='new_snippet'),
+    path('snippet/<str:pk>/', SnippetView.as_view(), name='snippet'),
 ]
