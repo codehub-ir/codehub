36cd6fe09e1921dc62a6038bdede16869b312885
lnxpy <lnxpylnxpy@gmail.com>
2021-11-29 01:57:59 +0330

    changing admin panel url due to conflicts

--- codehub/urls.py
+++ codehub/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminn/', admin.site.urls),
 
     # Accounting URLs
     path('', include('account.urls')),
