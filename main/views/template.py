9c125e7744fa3a70839c032c88675485740f5d1a
lnxpy <lnxpylnxpy@gmail.com>
2021-11-29 02:01:12 +0330

    TicketCreate and TicketDetail view designed

--- main/views/template.py
+++ main/views/template.py
@@ -1,10 +1,9 @@
 from django.views.generic import TemplateView, CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
 
-from main.models import Snippet
-
-from account.models import User
-
-from main.forms import SnippetCreateForm
+from main.models import Snippet, Ticket
+from main.forms import SnippetCreateForm, TicketCreateForm
 
 
 class HomeView(TemplateView):
@@ -26,3 +25,26 @@ class SnippetCreateView(CreateView):
 class SnippetView(DetailView):
     model = Snippet
     template_name = 'snippet.html'
+
+
+class TicketCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'create_ticket.html'
+    model = Ticket
+    form_class = TicketCreateForm
+
+    def form_valid(self, form):
+        user = self.request.user
+        form.instance.created_by = user
+        return super().form_valid(form)
+
+
+class TicketView(DetailView):
+    model = Ticket
+    template_name = 'ticket.html'
+
+    def get_context_data(self, **kwargs):
+        ticket = kwargs['object']
+        if ticket.created_by == self.request.user or ticket.is_valid == 'approved':
+            return super().get_context_data(**kwargs)
+        else:
+            raise Http404()
