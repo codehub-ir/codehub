812bf6ae798690a366ec80d14cee93f3efb40536
lnxpy <lnxpylnxpy@gmail.com>
2021-11-29 01:59:26 +0330

    ticket create and ticket detail urls added

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views.template import HomeView, SnippetCreateView, SnippetView
+from .views.template import HomeView, SnippetCreateView, SnippetView, TicketCreateView, TicketView
+
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('new/', SnippetCreateView.as_view(), name='new_snippet'),
+    path('new-snippet/', SnippetCreateView.as_view(), name='new_snippet'),
     path('snippet/<str:pk>/', SnippetView.as_view(), name='snippet'),
+
+    path('new-ticket/', TicketCreateView.as_view(), name='new_ticket'),
+    path('ticket/<str:pk>/<str:slug>',
+         TicketView.as_view(), name='ticket'),
 ]
