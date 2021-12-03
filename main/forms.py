08fb42b9652593181a60cfa9690224cfa78f6459
lnxpy <lnxpylnxpy@gmail.com>
2021-12-03 23:20:29 +0330

    comment creation form designed

--- main/forms.py
+++ main/forms.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext as _
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
-from .models import Snippet, Ticket
+from .models import Snippet, Ticket, Comment
 
 
 class SnippetCreateForm(ModelForm):
@@ -42,6 +42,23 @@ class TicketCreateForm(ModelForm):
     class Meta:
         model = Ticket
         fields = ('title', 'description', 'tags')
+
+    helper = FormHelper()
+    helper.add_input(Submit('submit', _('Create'),
+                     css_class='btn-primary crispy-form-item'))
+
+
+class CommentCreateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'crispy-form-item'
+            visible.field.widget.attrs['spellcheck'] = 'false'
+
+    class Meta:
+        model = Comment
+        fields = ('body',)
 
     helper = FormHelper()
     helper.add_input(Submit('submit', _('Create'),
