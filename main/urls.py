1c7771776ad896c768766df8891bceb2d28d143d
lnxpy <lnxpylnxpy@gmail.com>
2021-12-13 00:36:55 +0330

    [API] v1 docs generated in /api/v1/docs

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,24 @@ from django.views.generic import TemplateView
 
 from .views.template import HomeView, SnippetCreateView, SnippetView, TicketCreateView, TicketView
 from .views.api import SnippetCreateAPIView, SnippetAPIView
+from .constants import REDOC_DESCRIPTION
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='CodeHub Restful API Service Documentation',
+        default_version='v1',
+        description=REDOC_DESCRIPTION,
+        contact=openapi.Contact(email='lnxpylnxpy@gmail.com'),
+        license=openapi.License(name='MIT License'),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
 
@@ -19,11 +36,18 @@ urlpatterns = [
     # -- v1.0 --
     path('api/v1/snippet', SnippetCreateAPIView.as_view()),
     path('api/v1/snippet/<str:pk>', SnippetAPIView.as_view()),
+    path('api/v1/docs', schema_view.with_ui('redoc',
+         cache_timeout=0), name='api_docs_v1'),
 
     # static pages
     path(
         'download/',
         TemplateView.as_view(template_name='download.html'),
         name='download'
+    ),
+    path(
+        'development/',
+        TemplateView.as_view(template_name='development.html'),
+        name='development'
     ),
 ]
