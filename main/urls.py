f381a8db5c6210b4cb9d17a09618a93a2d192bb6
lnxpy <lnxpylnxpy@gmail.com>
2021-12-13 16:36:35 +0330

    creating ticket search results url

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views.template import HomeView, SnippetCreateView, SnippetView, TicketCreateView, TicketView
+from .views.template import HomeView, SnippetCreateView, SnippetView, TicketCreateView, TicketView, TicketSearchResultsView
 from .views.api import SnippetCreateAPIView, SnippetAPIView
 from .constants import REDOC_DESCRIPTION
 
@@ -31,6 +31,9 @@ urlpatterns = [
 
     path('new-ticket/', TicketCreateView.as_view(), name='new_ticket'),
     path('ticket/<str:pk>/<str:slug>', TicketView.as_view(), name='ticket'),
+
+    path('search/', TicketSearchResultsView.as_view(),
+         name='ticket_search_results'),
 
     # API URLs
     # -- v1.0 --
