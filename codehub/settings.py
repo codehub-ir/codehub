fa931b0efe02b79ceb38cdeba2e37f1050f7227a
lnxpy <lnxpylnxpy@gmail.com>
2021-12-26 04:17:04 +0330

    custom allEvent context processor added

--- codehub/settings.py
+++ codehub/settings.py
@@ -78,6 +78,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # custom event context
+                'main.context_processor.allEvents',
             ],
         },
     },
