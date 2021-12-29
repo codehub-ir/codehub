e0f6c46494efbf7d9b670141fd65ac1766ba197b
lnxpy <lnxpylnxpy@gmail.com>
2021-12-30 02:51:56 +0330

    dockerfile option added for Snippet.lang

--- main/constants.py
+++ main/constants.py
@@ -8,25 +8,26 @@ from django.utils.translation import gettext as _
 
 # lang choices for Snippet.lang
 LANGUAGES = (
-    ('arduino', 'Arduino'),      # + icon
-    ('bash', 'Bash'),            # + icon
-    ('c', 'C'),                  # + icon
-    ('cpp', 'C++'),              # + icon
-    ('csharp', 'C#'),            # + icon
-    ('css', 'CSS'),              # + icon
-    ('dart', 'Dart'),            # + icon
-    ('docker', 'DockerFile'),    # + icon
-    ('go', 'Go'),                # + icon
-    ('html', 'HTML'),            # + icon
-    ('java', 'Java'),            # + icon
-    ('js', 'JavaScript'),        # + icon
-    ('json', 'JSON'),            # + icon
-    ('lua', 'Lua'),              # + icon
-    ('md', 'markdown'),          # + icon
-    ('mysql', 'MySQL'),          # + icon
-    ('php', 'PHP'),              # + icon
-    ('python', 'Python'),        # + icon
-    ('rb', 'Ruby'),              # + icon
+    ('arduino', 'Arduino'),                  # + icon
+    ('bash', 'Bash'),                        # + icon
+    ('c', 'C'),                              # + icon
+    ('cpp', 'C++'),                          # + icon
+    ('csharp', 'C#'),                        # + icon
+    ('css', 'CSS'),                          # + icon
+    ('dart', 'Dart'),                        # + icon
+    ('docker', 'DockerFile'),                # + icon
+    ('docker-compose', 'DockerCompose'),     # + icon (same as docker.svg)
+    ('go', 'Go'),                            # + icon
+    ('html', 'HTML'),                        # + icon
+    ('java', 'Java'),                        # + icon
+    ('js', 'JavaScript'),                    # + icon
+    ('json', 'JSON'),                        # + icon
+    ('lua', 'Lua'),                          # + icon
+    ('md', 'markdown'),                      # + icon
+    ('mysql', 'MySQL'),                      # + icon
+    ('php', 'PHP'),                          # + icon
+    ('python', 'Python'),                    # + icon
+    ('rb', 'Ruby'),                          # + icon
 )
 
 # verification choices for Ticket
