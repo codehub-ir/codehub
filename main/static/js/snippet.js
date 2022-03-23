2f8a950ddc043dd190aa869e3012d95bac159d6d
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-03-23 20:00:52 +0430

    changed path to Snippet static directory

--- main/static/js/snippet.js
+++
@@ -1,9 +0,0 @@
-function copyElementText(id) {
-    var elem = document.createElement("textarea");
-    var host = window.location.host;
-    document.body.appendChild(elem);
-    elem.value = host + id;
-    elem.select();
-    document.execCommand("copy");
-    document.body.removeChild(elem);
-}
