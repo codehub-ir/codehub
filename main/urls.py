342984baa7005ce4961db76c3712bda3cd768f9e
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-03-23 20:05:55 +0430

    Ticket's and Snippet's API and Template URLs removed

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views.template import HomeView, SnippetCreateView, SnippetView, TicketCreateView, TicketView, TicketSearchResultsView
-from .views.api import SnippetCreateAPIView, SnippetAPIView, ListEventsAPIView
+from .views import HomeView, ListEventsAPIView
 from .constants import REDOC_DESCRIPTION
 
 from rest_framework import permissions
@@ -26,19 +25,9 @@ urlpatterns = [
 
     # Template URLs
     path('', HomeView.as_view(), name='home'),
-    path('new-snippet/', SnippetCreateView.as_view(), name='new_snippet'),
-    path('snippet/<str:pk>/', SnippetView.as_view(), name='snippet'),
-
-    path('new-ticket/', TicketCreateView.as_view(), name='new_ticket'),
-    path('ticket/<str:pk>/<str:slug>', TicketView.as_view(), name='ticket'),
-
-    path('search/', TicketSearchResultsView.as_view(),
-         name='ticket_search_results'),
 
     # API URLs
     # -- v1.0 --
-    path('api/v1/snippet', SnippetCreateAPIView.as_view()),
-    path('api/v1/snippet/<str:pk>', SnippetAPIView.as_view()),
     path('api/v1/event', ListEventsAPIView.as_view()),
     path('api/v1/docs', schema_view.with_ui('redoc',
          cache_timeout=0), name='api_docs_v1'),
