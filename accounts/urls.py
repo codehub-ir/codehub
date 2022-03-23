fdd34617ae3359a1a7679bff3b4dc554a15e8805
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-03-23 20:13:21 +0430

    login path removed (using besic one)

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('password/', CustomPasswordChangeView.as_view(), name='change_password'),
-    path('login/', CustomLoginView.as_view(), name='login'),
 ]
