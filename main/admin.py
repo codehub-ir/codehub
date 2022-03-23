b31515ced543961812726ae6f4f021fc9cda9cba
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-03-23 20:02:32 +0430

    minor changes

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from .models import Comment, Snippet, Tag, Ticket, Event
+from snippets.models import Snippet
+from tickets.models import Ticket, Tag, Comment
+from .models import Event
+
 
 admin.site.register(Snippet)
 admin.site.register(Ticket)
-admin.site.register(Tag)
 admin.site.register(Comment)
+admin.site.register(Tag)
 admin.site.register(Event)
