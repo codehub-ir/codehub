b378bb4b6451319340c917143dccb21872be4103
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-03-23 20:05:21 +0430

    SnippetSerializer removed

--- main/serializers.py
+++ main/serializers.py
@@ -1,17 +1,9 @@
 from rest_framework import serializers
-from .models import Snippet, Event
-
-
-class SnippetSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Snippet
-        fields = ('id', 'title', 'description', 'body',
-                  'lang', 'created_by', 'created_on')
+from .models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Snippet
+        model = Event
         fields = ('title', 'body', 'created_on')
